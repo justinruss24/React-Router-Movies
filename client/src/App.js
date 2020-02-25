ee53854fbffbdce453f3facb82b3e0b3f94534bd
Justin Russell <Justin@Justins-MacBook.local>
2020-02-25 13:43:25 -0800

    mvp done

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <Route exact path="/">
         <MovieList />
       </Route>
-      <Route path="/movies/:title">
+      <Route path="/movies/:id">
         <Movie />
       </Route>
     </div>
